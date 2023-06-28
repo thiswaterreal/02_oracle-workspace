27290fd83779ad8defa6217858bde89614f61f4f
thiswaterreal <thiswaterreal@gmail.com>
2023-06-28 20:41:11 +0900

    commit

--- 01_DQL(SELECT)/04_JOIN_KH계정.sql
+++ 01_DQL(SELECT)/04_JOIN_KH계정.sql
@@ -48,7 +48,7 @@ FROM EMPLOYEE;
 
 SELECT JOB_CODE, JOB_NAME
 FROM JOB;
-
+--=========================================< 등가조인 >================================================
 /*
     1.  등가조인 (EQUAL JOIN) / 내부조인 (INNER JOIN)
         연결시키는 컬럼의 값이 일치하는 행들만 조인되어서 조회 (== 일치하는 값이 없는 행은 조회에서 제외)
@@ -58,15 +58,15 @@ FROM JOB;
 --      FROM절에 조회하고자 하는 테이블들을 나열(, 구분자로)
 --      WHERE절에 매칭시킬 컬럼(연결고리)에 대한 조건을 제시함
 
--- 1) 연결한 두 컬럼명이 다른 경우 (EMPLOYEE : DEPT_CODE / DEPARTMENT : DEPT_ID)
+-- 1) 연결한 두 컬럼명이 '다른' 경우 (EMPLOYEE : DEPT_CODE / DEPARTMENT : DEPT_ID)
 --    사번, 사원명, 부서코드, 부서명을 같이 조회
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, DEPT_TITLE
 FROM EMPLOYEE, DEPARTMENT
 WHERE DEPT_CODE = DEPT_ID;
--- 일치하는 값이 없는 행은 조회에서 제외된거 확인 가능
+-- 일치하는 값이 없는 행은 조회에서 제외된거 확인 가능 -- 21개
 -- DEPT_CODE 가 NULL인 사원 조회 X, DEPT_ID가 D3, D4, D7 조회 X  --(23명인데 21명 조회됨)
 
--- 2) 연결한 두 컬럼명이 같은 경우 (EMPLOYEE : JOB_CODE /  JOB : JOB_CODE)
+-- 2) 연결한 두 컬럼명이 '같은' 경우 (EMPLOYEE : JOB_CODE /  JOB : JOB_CODE)
 --    사번, 사원명, 직급코드, 직급명
 SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
 FROM EMPLOYEE, JOB
@@ -89,14 +89,14 @@ WHERE E.JOB_CODE = J.JOB_CODE;
 -- JOIN절에 같이 조회하고자 하는 테이블 기술 + 매칭시킬 컬럼에 대한 조건도 기술
 -- JOIN USING, JOIN ON
 
--- 1) 연결할 두 컬럼명이 다른 경우 (EMPLOYEE : DEPT_CODE, DEPARTMENT : DEPT_ID)
---    오로지 JOIN ON 구문으로만 사용 가능!!
+-- 1) 연결할 두 컬럼명이 '다른' 경우 (EMPLOYEE : DEPT_CODE, DEPARTMENT : DEPT_ID)
+--    오로지!! JOIN ON 구문으로만 사용 가능!!
 --    사번, 사원명, 부서코드, 부서명
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, DEPT_TITLE
 FROM EMPLOYEE
 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
 
--- 2) 연결할 두 컬럼명이 같은 경우 (EMPLOYEE : JOB_CODE, JOB : JOB_CODE)
+-- 2) 연결할 두 컬럼명이 '같은' 경우 (EMPLOYEE : JOB_CODE, JOB : JOB_CODE)
 --    JOIN ON, JOIN USING 구문 사용 가능!!
 --    사번, 사원명, 직급코드, 직급명
 SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
@@ -126,7 +126,7 @@ NATURAL JOIN JOB;
 SELECT EMP_NAME, JOB_NAME, SALARY
 FROM EMPLOYEE E, JOB J
 WHERE E.JOB_CODE = J.JOB_CODE
-AND J.JOB_NAME = '대리';
+AND JOB_NAME = '대리';
 
 -- >> ANSI 구문
 SELECT EMP_NAME, JOB_NAME, SALARY
@@ -135,24 +135,24 @@ JOIN JOB USING (JOB_CODE)   --2
 WHERE JOB_NAME = '대리';    --3
 
 ---------------------------------------- 실습문제 -----------------------------------------
-SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)               /연결고리
 SELECT * FROM DEPARTMENT;   --(DEPT_ID)
 
--- 1. 부서가 인사관리부인 사원들의 사번, 이름, 보너스 조회    (컬럼명 달라)
+-- 1. 부서가 인사관리부인 사원들의 사번, 이름, 보너스 조회
 -- >> 오라클 전용 구문
-SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+SELECT EMP_ID, EMP_NAME, BONUS
 FROM EMPLOYEE, DEPARTMENT
 WHERE DEPT_CODE = DEPT_ID
 AND DEPT_TITLE = '인사관리부';
 
 -- >> ANSI 구문
-SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+SELECT EMP_ID, EMP_NAME, BONUS
 FROM EMPLOYEE
-JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
-AND DEPT_TITLE = '인사관리부';
+JOIN DEPARTMENT ON (DEPT_CODE =  DEPT_ID)
+WHERE DEPT_TITLE = '인사관리부';
 
 ---------------------------------------------
-SELECT * FROM DEPARTMENT;   --(LOCATION_ID)
+SELECT * FROM DEPARTMENT;   --(LOCATION_ID)             / 연결고리
 SELECT * FROM LOCATION;     --(LOCAL_CODE)
 
 -- 2. DEPARTMENT 와 LOCATION 을 참고해서 전체 부서의 부서코드, 부서명, 지역코드, 지역명 조회 (컬럼명 달라)
@@ -167,10 +167,10 @@ FROM DEPARTMENT
 JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE);
 
 ---------------------------------------------
-SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)              / 연결고리
 SELECT * FROM DEPARTMENT;   --(DEPT_ID)
 
--- 3. 보너스를 받는 사원들의 사번, 사원명, 보너스, 부서명 조회 (컬럼명 달라)
+-- 3. 보너스를 받는 사원들의 사번, 사원명, 보너스, 부서명 조회
 -- >> 오라클 전용 구문
 SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
 FROM EMPLOYEE, DEPARTMENT
@@ -198,7 +198,7 @@ AND DEPT_TITLE != '총무부';
 SELECT EMP_NAME, SALARY, DEPT_TITLE
 FROM EMPLOYEE
 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
-WHERE DEPT_TITLE != '총무부';
+AND DEPT_TITLE != '총무부';
 
 -- 지금 현재 DEPT_CODE가 NULL인 것은 나오고 있지 않음
 
@@ -213,8 +213,8 @@ WHERE DEPT_TITLE != '총무부';
 SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
 FROM EMPLOYEE
 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
--- 부서 배치가 아직 안된 사원 2명에 대한 정보가 조회 X (EMPLOYEE 기준)
--- 부서에 배정된 사원이없는 부서 같은 경우도 조회 X (DEPARTMENT 기준)
+-- 부서 배치가 아직 안된 사원 2명에 대한 정보가 조회 X (EMPLOYEE 기준) (현재 21개)
+-- 부서에 사원이 아무도 없는 부서 같은 경우도 조회 X (DEPARTMENT 기준) (현재 21개)
 
 -- 1) LEFT [OUTER] JOIN : 두 테이블 중 왼편에 기술된 테이블 기준으로 JOIN
 -- >> ANSI 구문
@@ -283,12 +283,24 @@ SELECT EMP_ID, EMP_NAME, MANAGER_ID  FROM EMPLOYEE;
 --      사수의 사번, 사수명, 사수부서코드       => EMPLOYEE M
 
 -- >> 오라클 전용 구문
+/*
+SELECT E.EMP_ID, E.EMP_NAME, E.DEPT_CODE,
+       M.EMP_ID, M.EMP_NAME, M.DEPT_CODE
+FROM EMPLOYEE E, EMPLOYEE M
+WHERE E.MANAGER_ID = M.EMP_ID;
+*/
 SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서코드",
        M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
 FROM EMPLOYEE E, EMPLOYEE M
 WHERE E.MANAGER_ID = M.EMP_ID;
 
 -- >> ANSI 구문
+/*
+SELECT E.EMP_ID, E.EMP_NAME, E.DEPT_CODE,
+       M.EMP_ID, M.EMP_NAME, M.DEPT_CODE
+FROM EMPLOYEE E
+JOIN EMPLOYEE M ON (E.MANAGER_ID = M.EMP_ID);
+*/
 SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서코드",
        M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
 FROM EMPLOYEE E
@@ -300,7 +312,8 @@ SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서�
        M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
 FROM EMPLOYEE E
 LEFT JOIN EMPLOYEE M ON (E.MANAGER_ID = M.EMP_ID);
--- 이제, 사수 없는 사람들까지 모두 나옴 (LEFT JOIN)
+-- 이제, 사수 없는 사람들까지 모두 나옴 (LEFT JOIN : 23개)
+-- 싹 다 나오기를 원하면 (FULL JOIN : 40개)
 
 --================================== < 다중 조인 > ========================================
 /*
@@ -321,7 +334,7 @@ AND E.JOB_CODE = J.JOB_CODE;
 
 -- >> ANSI 구문
 SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME
-FROM EMPLOYEE E
+FROM EMPLOYEE
 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
 JOIN JOB USING(JOB_CODE);
 
@@ -348,43 +361,60 @@ JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE);
 -- 1. 사번, 사원명, 부서명, 지역명, 국가명 조회 (EMP, DEP, LOC, NAT 조인)
 SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
 SELECT * FROM DEPARTMENT;   --(DEOT_ID      LOCATION_ID)
-SELECT * FROM LOCATION;     --(             LOCAL_CODE,     NATIONAL_NAME)
-SELECT * FROM NATIONAL;     --(                             NATIONAL_NAME)
+SELECT * FROM LOCATION;     --(             LOCAL_CODE,     NATIONAL_CODE)
+SELECT * FROM NATIONAL;     --(                             NATIONAL_CODE)
 
 -- >> 오라클 전용구문
-SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, NATIONAL_NAME
-FROM EMPLOYEE E, DEPARTMENT D, LOCATION L, NATIONAL N
-WHERE E.DEPT_CODE = D.DEPT_ID
-AND D.LOCATION_ID = L.LOCAL_CODE
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, L.NATIONAL_CODE
+FROM EMPLOYEE, DEPARTMENT, LOCATION L, NATIONAL N
+WHERE DEPT_CODE = DEPT_ID
+AND LOCATION_ID = LOCAL_CODE
 AND L.NATIONAL_CODE = N.NATIONAL_CODE;
 
 -- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, NATIONAL_CODE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE)
+JOIN NATIONAL USING (NATIONAL_CODE);
+
+-- >> ANSI 구문
+/* 별칭 붙인 버전
 SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, NATIONAL_NAME
 FROM EMPLOYEE E
 JOIN DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
 JOIN LOCATION L ON (D.LOCATION_ID = L.LOCAL_CODE)
 JOIN NATIONAL N USING (NATIONAL_CODE);
---JOIN NATIONAL N ON (L.NATIONAL_CODE = N.NATIONAL_NAME);
-
+*/
 ----------------------------------------------
 -- 2. 사번, 사원명, 부서명, 직급명, 지역명, 국가명, 해당 급여등급에서 받을 수 있는 최대금액 조회
 SELECT * FROM JOB;          --(JOB_CODE)
-SELECT * FROM EMPLOYEE;     --(JOB_CODE, DEPT_CODE)
-SELECT * FROM DEPARTMENT;   --(            DEPT_ID,   LOCATION_ID)
-SELECT * FROM LOCATION;     --(                       LOCAL_CODE,  NATIONAL_NAME)
-SELECT * FROM NATIONAL;     --(                                    NATIONAL_NAME)
-SELECT * FROM SAL_GRADE;    --  SAL_LEVEL
+SELECT * FROM EMPLOYEE;     --(JOB_CODE  DEPT_CODE                                SAL_LEVEL)
+SELECT * FROM DEPARTMENT;   --(            DEPT_ID    LOCATION_ID               )
+SELECT * FROM LOCATION;     --(                       LOCAL_CODE   NATIONAL_CODE)
+SELECT * FROM NATIONAL;     --(                                    NATIONAL_CODE)
+SELECT * FROM SAL_GRADE;    --(                                                   SAL_LEVEL)
 
 --> 오라클 전용 구문
 SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, LOCAL_NAME, NATIONAL_NAME, MAX_SAL
-FROM EMPLOYEE E, DEPARTMENT, JOB J, LOCATION L, NATIONAL N, SAL_GRADE S
-WHERE DEPT_CODE = DEPT_ID
-AND E.JOB_CODE = J.JOB_CODE
+FROM JOB J, EMPLOYEE E, DEPARTMENT, LOCATION L, NATIONAL N, SAL_GRADE S
+WHERE J.JOB_CODE = E.JOB_CODE
+AND DEPT_CODE = DEPT_ID
 AND LOCATION_ID = LOCAL_CODE
 AND L.NATIONAL_CODE = N.NATIONAL_CODE
 AND E.SAL_LEVEL = S.SAL_LEVEL;
 
 --> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, LOCAL_NAME, NATIONAL_NAME, MAX_SAL
+FROM JOB
+JOIN EMPLOYEE USING (JOB_CODE)
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE)
+JOIN NATIONAL USING (NATIONAL_CODE)
+JOIN SAL_GRADE USING (SAL_LEVEL);    -- NATIONAL 이랑 조인이 아니라 DEPARTMENT랑 조인해야하는데.. 순서 중요한데.. 왜 돼?
+
+-- >> ANSI 구문
+/* 별칭 붙인 버전
 SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, LOCAL_NAME, NATIONAL_NAME, MAX_SAL
 FROM EMPLOYEE
 JOIN DEPARTMENT D ON (DEPT_CODE = DEPT_ID)
@@ -392,20 +422,4 @@ JOIN JOB USING(JOB_CODE)
 JOIN LOCATION L ON (D.LOCATION_ID = L.LOCAL_CODE)
 JOIN NATIONAL N USING(NATIONAL_CODE)
 JOIN SAL_GRADE USING(SAL_LEVEL);
-
-/**/
----------------------------------- 실습 문제-------------------------------------
--- 1. 사번, 사원명, 부서명, 지역명, 국가명 조회 (EMP, DEP, LOC, NAT 조인)
-SELECT * FROM EMPLOYEE; -- DEPT_CODE
-SELECT * FROM DEPARTMENT; -- DEPT_ID  LOCATION_ID(L1)
-SELECT * FROM LOCATION; --              LOCAL_CODE NATIONAL_CODE
-SELECT * FROM NATIONAL; --                         NATIONAL_CODE
-
-
--- 2. 사번, 사원명, 부서명, 직급명, 지역명, 국가명, 해당 급여등급에서 받을 수 있는 최대금액 조회
-SELECT * FROM EMPLOYEE; -- DEPT_CODE
-SELECT * FROM DEPARTMENT; -- DEPT_ID LOCATION_ID
-SELECT * FROM JOB; -- JOB_CODE로 동일함
-SELECT * FROM LOCATION; --            LOCAL_CODE
-SELECT * FROM SAL_GRADE; -- SAL_LEVEL 로 동일함
-
+*/
