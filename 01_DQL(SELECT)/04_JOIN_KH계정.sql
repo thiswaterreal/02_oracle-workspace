d1bbb786c46a48bbd593832f389fa8e7c4ace817
thiswaterreal <thiswaterreal@gmail.com>
2023-06-28 16:46:02 +0900

    commit

---
+++ 01_DQL(SELECT)/04_JOIN_KH계정.sql
@@ -0,0 +1,411 @@
+SELECT * FROM EMPLOYEE
+WHERE EMP_NAME = '선동일'; --D9?
+SELECT * FROM DEPARTMENT 
+WHERE DEPT_ID =  'D9'; --L1?
+SELECT * FROM LOCATION 
+WHERE LOCAL_CODE = 'L1'; --KO?
+SELECT* FROM NATIONAL 
+WHERE NATIONAL_CODE = 'KO';
+
+/*
+    < JOIN >
+    두 개 이상의 테이블에서 데이터를 조회하고자 할 때 사용되는 구문
+    조회 결과는 하나의 결과물(RESULT SET)로 나옴
+    
+    관계형 데이터베이스는 최소한의 데이터로 각각의 테이블에 데이터를 담고 있음 (중복을 최소화하기 위해 최대한 쪼개서 관리함)
+    
+    -- 어떤 사원이 어떤 부서에 속해있는지 궁금함! 코드말고(D9).. 이름으로(총무부)..
+    
+    => 관계형 데이터베이스에서 SQL문을 이용한 데이블간의 '관계'를 맺는 방법
+        (무작정 다 조회를 해오는게 아니라 각 테이블간 연결고리로써의 데이터를 매칭해서 조회 시켜야함!)
+        
+                JOIN : 크게 '오라클 전용구문' 과 'ANSI 구문' (ANSI == 미국국립표준협 <= 아스키코드표 만드는 단체)
+                                        [ JOIN 용어 정리 ]
+                오라클 전용구문                    |                   ANSI 구문
+    ----------------------------------------------------------------------------------------------------
+                    *등가조인                     |        내부 조인 ( [INNER] JOIN ) => JOIN USING / ON
+                ( EQUAL JOIN )                   |        자연 조인 ( NATURAL JOIN ) => JOIN USING
+    ----------------------------------------------------------------------------------------------------
+                    포괄조인                      |        왼쪽 외부조인 ( LEFT OUTER JOIN )
+                ( LEFT OUTER )                   |        오른쪽 외부조인 ( RIGHT OUTER JOIN )
+                ( RIGHT OUTER )                  |        전체 외부조인 ( FULL OUTER JOIN )
+    ----------------------------------------------------------------------------------------------------
+                   *자체조인 ( SELF JOIN )        |        JOIN ON
+                비등가 조인 ( NON EQUAL JOIN )     |
+    ----------------------------------------------------------------------------------------------------
+*/
+
+-- 전체 사원들의 사번, 사원명, 부서코드, 부서명 조회하고자 할 때
+SELECT EMP_ID, EMP_NAME, DEPT_CODE
+FROM EMPLOYEE;
+
+SELECT DEPT_ID, DEPT_TITLE
+FROM DEPARTMENT;
+
+-- 전체 사원들의 사번, 사원명, 직급코드, 직급명 조회하고자 할 때
+SELECT EMP_ID, EMP_NAME, JOB_CODE
+FROM EMPLOYEE;
+
+SELECT JOB_CODE, JOB_NAME
+FROM JOB;
+
+/*
+    1.  등가조인 (EQUAL JOIN) / 내부조인 (INNER JOIN)
+        연결시키는 컬럼의 값이 일치하는 행들만 조인되어서 조회 (== 일치하는 값이 없는 행은 조회에서 제외)
+*/
+
+-- >>   오라클 전용구문
+--      FROM절에 조회하고자 하는 테이블들을 나열(, 구분자로)
+--      WHERE절에 매칭시킬 컬럼(연결고리)에 대한 조건을 제시함
+
+-- 1) 연결한 두 컬럼명이 다른 경우 (EMPLOYEE : DEPT_CODE / DEPARTMENT : DEPT_ID)
+--    사번, 사원명, 부서코드, 부서명을 같이 조회
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID;
+-- 일치하는 값이 없는 행은 조회에서 제외된거 확인 가능
+-- DEPT_CODE 가 NULL인 사원 조회 X, DEPT_ID가 D3, D4, D7 조회 X  --(23명인데 21명 조회됨)
+
+-- 2) 연결한 두 컬럼명이 같은 경우 (EMPLOYEE : JOB_CODE /  JOB : JOB_CODE)
+--    사번, 사원명, 직급코드, 직급명
+SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+FROM EMPLOYEE, JOB
+WHERE JOB_CODE = JOB_CODE;
+-- ambiguously : 애매하다, 모호하다
+
+-- 해결방법 1) 테이블명을 이용하는 방법
+SELECT EMP_ID, EMP_NAME, EMPLOYEE.JOB_CODE, JOB_NAME
+FROM EMPLOYEE, JOB
+WHERE EMPLOYEE.JOB_CODE = JOB.JOB_CODE;
+
+-- 해결방법 2) 테이블에 별칭을 부여해서 이용하는 방법    **주로 요놈 사용**
+SELECT EMP_ID, EMP_NAME, E.JOB_CODE, JOB_NAME
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE = J.JOB_CODE;
+
+
+-- >> ANSI 구문
+-- FROM절에 기준이 되는 테이블을 하나 기술 한 후,
+-- JOIN절에 같이 조회하고자 하는 테이블 기술 + 매칭시킬 컬럼에 대한 조건도 기술
+-- JOIN USING, JOIN ON
+
+-- 1) 연결할 두 컬럼명이 다른 경우 (EMPLOYEE : DEPT_CODE, DEPARTMENT : DEPT_ID)
+--    오로지 JOIN ON 구문으로만 사용 가능!!
+--    사번, 사원명, 부서코드, 부서명
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 2) 연결할 두 컬럼명이 같은 경우 (EMPLOYEE : JOB_CODE, JOB : JOB_CODE)
+--    JOIN ON, JOIN USING 구문 사용 가능!!
+--    사번, 사원명, 직급코드, 직급명
+SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+FROM EMPLOYEE
+JOIN JOB ON (JOB_CODE = JOB_CODE);
+-- ambiguously 오류!!
+
+-- 해결방법 1) 테이블명 또는 별칭을 이용해서 하는 방법
+SELECT EMP_ID, EMP_NAME, E.JOB_CODE, JOB_NAME
+FROM EMPLOYEE E
+JOIN JOB J ON (E.JOB_CODE = J.JOB_CODE);
+
+-- 해결방법 2) JOIN USING 구문 사용하는 방법 (두 컬럼명이 일치할때만 사용 가능)   **만약 ANSI 쓰면 요놈**
+SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+FROM EMPLOYEE
+JOIN JOB USING (JOB_CODE);
+
+--------------- [참고사항] ----------------
+-- 자연 조인 (NATURAL JOIN) : 각 테이블마다 동일한 컬럼이 한개만 존재할 경우
+SELECT EMP_ID, EMP_NAME, JOB_CODE, JOB_NAME
+FROM EMPLOYEE
+NATURAL JOIN JOB;
+
+-- 추가적인 조건도 당근 제시 가능!!
+-- 직급이 대리인 사원의 이름, 직급명, 급여 조회
+-- >> 오라클 전용 구문
+SELECT EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE = J.JOB_CODE
+AND J.JOB_NAME = '대리';
+
+-- >> ANSI 구문
+SELECT EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE   --1
+JOIN JOB USING (JOB_CODE)   --2
+WHERE JOB_NAME = '대리';    --3
+
+---------------------------------------- 실습문제 -----------------------------------------
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(DEPT_ID)
+
+-- 1. 부서가 인사관리부인 사원들의 사번, 이름, 보너스 조회    (컬럼명 달라)
+-- >> 오라클 전용 구문
+SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+AND DEPT_TITLE = '인사관리부';
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+AND DEPT_TITLE = '인사관리부';
+
+---------------------------------------------
+SELECT * FROM DEPARTMENT;   --(LOCATION_ID)
+SELECT * FROM LOCATION;     --(LOCAL_CODE)
+
+-- 2. DEPARTMENT 와 LOCATION 을 참고해서 전체 부서의 부서코드, 부서명, 지역코드, 지역명 조회 (컬럼명 달라)
+-- >> 오라클 전용 구문
+SELECT DEPT_ID, DEPT_TITLE, LOCATION_ID, LOCAL_NAME
+FROM DEPARTMENT, LOCATION
+WHERE LOCATION_ID = LOCAL_CODE;
+
+-- >> ANSI 구문
+SELECT DEPT_ID, DEPT_TITLE, LOCATION_ID, LOCAL_NAME
+FROM DEPARTMENT
+JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE);
+
+---------------------------------------------
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(DEPT_ID)
+
+-- 3. 보너스를 받는 사원들의 사번, 사원명, 보너스, 부서명 조회 (컬럼명 달라)
+-- >> 오라클 전용 구문
+SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+AND BONUS IS NOT NULL;
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, BONUS, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE BONUS IS NOT NULL;
+
+---------------------------------------------
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(DEPT_ID)
+
+-- 4. 부서가 총무부가 아닌 사원들의 사원명, 급여, 부서명 조회 (컬럼명 달라)
+-- >> 오라클 전용 구문
+SELECT EMP_NAME, SALARY, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+AND DEPT_TITLE != '총무부';
+
+-- >> ANSI 구문
+SELECT EMP_NAME, SALARY, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE DEPT_TITLE != '총무부';
+
+-- 지금 현재 DEPT_CODE가 NULL인 것은 나오고 있지 않음
+
+--================================== < 포괄 조인 > ========================================
+/*
+    2. 포괄 조인 / 외부 조인 ( OUTER JOIN )
+    두 테이블 간의 JOIN 시, 일치하지 않는 행도 포함시켜서 조회 가능
+    단, 반드시 LEFT / RIGHT 지정해야됨 (기준이 되는 테이블 지정)
+*/
+
+-- 사원명, 부서명, 급여, 연봉
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- 부서 배치가 아직 안된 사원 2명에 대한 정보가 조회 X (EMPLOYEE 기준)
+-- 부서에 배정된 사원이없는 부서 같은 경우도 조회 X (DEPARTMENT 기준)
+
+-- 1) LEFT [OUTER] JOIN : 두 테이블 중 왼편에 기술된 테이블 기준으로 JOIN
+-- >> ANSI 구문
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE   -- EMPLOYEE에 있는건 무조건 다 나오는것임 (왼편이니까)
+LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- 부서 배치를 받지 않았던 2명의 사원 정보까지 모두 조회 됨    --(23개)
+
+-- >> 오라클 전용 구문
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID(+);   -- 기준으로 삼고자 하는 테이블의 반대편 컬럼 뒤에 (+) 붙이기
+
+-- 2) RIGHT [OUTER] JOIN : 두 테이블 중 오른편에 기술된 테이블을 기준으로 JOIN
+-- >> ANSI 구문
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE
+RIGHT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- 부서에 배정된 사원이없는 부서까지 모두 조회 됨   --(24개)
+
+-- >> 오라클 전용 구문
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE(+) = DEPT_ID;   -- 기준으로 삼고자 하는 테이블의 반대편 컬럼 뒤에 (+) 붙이기
+
+-- 3) FULL [OUTER] JOIN : 두 테이블을 가진 모든 행을 조회할 수 있음 (단, 오라클 전용구문으로는 안됨!!)
+-- >> ANSI 구문
+SELECT EMP_NAME, DEPT_TITLE, SALARY, SALARY*12
+FROM EMPLOYEE
+FULL JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);  --(26개)
+
+--================================== < 비등가 조인 > ====================참고==================
+/*
+    3. 비등가 조인 (NON EQUAL JOIN) <= 얘는 그냥 참고용
+    매칭시킬 컬럼에 대한 조건 작성시 '=(등호)' 를 사용하지 않는 조인문
+    ANSI 구문으로는 JOIN ON만 사용 가능
+*/
+
+SELECT EMP_NAME, SALARY, SAL_LEVEL
+FROM EMPLOYEE;
+
+SELECT * FROM SAL_GRADE;
+
+-- 사원명, 급여, 최대 월급 한도
+
+-- >> 오라클 전용구문
+SELECT EMP_NAME, SALARY, MAX_SAL
+FROM EMPLOYEE, SAL_GRADE
+--WHERE SALARY >= MIN_SAL AND SALARY <= MAX_SAL;
+WHERE SALARY BETWEEN MIN_SAL AND MAX_SAL;
+
+-- >> ANSI 구문
+SELECT EMP_NAME, SALARY, MAX_SAL
+FROM EMPLOYEE
+JOIN SAL_GRADE ON (SALARY BETWEEN MIN_SAL AND MAX_SAL);
+
+--================================== < 자체 조인 > ========================================
+/*
+    4. 자체 조인 (SELF JOIN)
+    같은 테이블을 다시 한번 더 조인하는 경우
+*/
+
+SELECT EMP_ID, EMP_NAME, MANAGER_ID  FROM EMPLOYEE;
+
+-- 전체 사원의 사번, 사원명, 사원부서코드       => EMPLOYEE E
+--      사수의 사번, 사수명, 사수부서코드       => EMPLOYEE M
+
+-- >> 오라클 전용 구문
+SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서코드",
+       M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
+FROM EMPLOYEE E, EMPLOYEE M
+WHERE E.MANAGER_ID = M.EMP_ID;
+
+-- >> ANSI 구문
+SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서코드",
+       M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
+FROM EMPLOYEE E
+JOIN EMPLOYEE M ON (E.MANAGER_ID = M.EMP_ID);
+-- 단, 사수 없는 사람들은 안나옴
+
+-- >> ANSI 구문
+SELECT E.EMP_ID AS "사원사번", E.EMP_NAME AS "사원명", E.DEPT_CODE AS "사원부서코드",
+       M.EMP_ID AS "사수사번", M.EMP_NAME AS "사수명", M.DEPT_CODE AS "사수부서코드"
+FROM EMPLOYEE E
+LEFT JOIN EMPLOYEE M ON (E.MANAGER_ID = M.EMP_ID);
+-- 이제, 사수 없는 사람들까지 모두 나옴 (LEFT JOIN)
+
+--================================== < 다중 조인 > ========================================
+/*
+    < 다중 조인 >
+    2개 이상의 테이블을 가지고 JOIN 할 때
+*/
+
+-- 사번, 사원명, 부서명, 직급명 조회
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE, JOB_CODE)
+SELECT * FROM DEPARTMENT;   --(DEPT_ID)
+SELECT * FROM JOB;          --           (JOB_CODE)
+
+-- >> 오라클 전용구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME
+FROM EMPLOYEE E, DEPARTMENT, JOB J
+WHERE DEPT_CODE = DEPT_ID
+AND E.JOB_CODE = J.JOB_CODE;
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN JOB USING(JOB_CODE);
+
+-----------------------------------------------
+-- 사번, 사원명, 부서명, 지역명
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(DEPT_ID,   LOCATION_ID)
+SELECT * FROM LOCATION;     --(           LOCAL_CODE)
+
+-- >> 오라클 전용 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME
+FROM EMPLOYEE, DEPARTMENT, LOCATION
+WHERE DEPT_CODE = DEPT_ID
+AND LOCATION_ID = LOCAL_CODE;
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN LOCATION ON (LOCATION_ID = LOCAL_CODE);
+-- 순서중요!! EMPLOYEE - DEPARTMENT - LOACTION
+
+----------------------------------------- 실습문제 ----------------------------------------
+-- 1. 사번, 사원명, 부서명, 지역명, 국가명 조회 (EMP, DEP, LOC, NAT 조인)
+SELECT * FROM EMPLOYEE;     --(DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(DEOT_ID      LOCATION_ID)
+SELECT * FROM LOCATION;     --(             LOCAL_CODE,     NATIONAL_NAME)
+SELECT * FROM NATIONAL;     --(                             NATIONAL_NAME)
+
+-- >> 오라클 전용구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, NATIONAL_NAME
+FROM EMPLOYEE E, DEPARTMENT D, LOCATION L, NATIONAL N
+WHERE E.DEPT_CODE = D.DEPT_ID
+AND D.LOCATION_ID = L.LOCAL_CODE
+AND L.NATIONAL_CODE = N.NATIONAL_CODE;
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, LOCAL_NAME, NATIONAL_NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
+JOIN LOCATION L ON (D.LOCATION_ID = L.LOCAL_CODE)
+JOIN NATIONAL N USING (NATIONAL_CODE);
+--JOIN NATIONAL N ON (L.NATIONAL_CODE = N.NATIONAL_NAME);
+
+----------------------------------------------
+-- 2. 사번, 사원명, 부서명, 직급명, 지역명, 국가명, 해당 급여등급에서 받을 수 있는 최대금액 조회
+SELECT * FROM JOB;          --(JOB_CODE)
+SELECT * FROM EMPLOYEE;     --(JOB_CODE, DEPT_CODE)
+SELECT * FROM DEPARTMENT;   --(            DEPT_ID,   LOCATION_ID)
+SELECT * FROM LOCATION;     --(                       LOCAL_CODE,  NATIONAL_NAME)
+SELECT * FROM NATIONAL;     --(                                    NATIONAL_NAME)
+SELECT * FROM SAL_GRADE;    --  SAL_LEVEL
+
+--> 오라클 전용 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, LOCAL_NAME, NATIONAL_NAME, MAX_SAL
+FROM EMPLOYEE E, DEPARTMENT, JOB J, LOCATION L, NATIONAL N, SAL_GRADE S
+WHERE DEPT_CODE = DEPT_ID
+AND E.JOB_CODE = J.JOB_CODE
+AND LOCATION_ID = LOCAL_CODE
+AND L.NATIONAL_CODE = N.NATIONAL_CODE
+AND E.SAL_LEVEL = S.SAL_LEVEL;
+
+--> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, LOCAL_NAME, NATIONAL_NAME, MAX_SAL
+FROM EMPLOYEE
+JOIN DEPARTMENT D ON (DEPT_CODE = DEPT_ID)
+JOIN JOB USING(JOB_CODE)
+JOIN LOCATION L ON (D.LOCATION_ID = L.LOCAL_CODE)
+JOIN NATIONAL N USING(NATIONAL_CODE)
+JOIN SAL_GRADE USING(SAL_LEVEL);
+
+/**/
+---------------------------------- 실습 문제-------------------------------------
+-- 1. 사번, 사원명, 부서명, 지역명, 국가명 조회 (EMP, DEP, LOC, NAT 조인)
+SELECT * FROM EMPLOYEE; -- DEPT_CODE
+SELECT * FROM DEPARTMENT; -- DEPT_ID  LOCATION_ID(L1)
+SELECT * FROM LOCATION; --              LOCAL_CODE NATIONAL_CODE
+SELECT * FROM NATIONAL; --                         NATIONAL_CODE
+
+
+-- 2. 사번, 사원명, 부서명, 직급명, 지역명, 국가명, 해당 급여등급에서 받을 수 있는 최대금액 조회
+SELECT * FROM EMPLOYEE; -- DEPT_CODE
+SELECT * FROM DEPARTMENT; -- DEPT_ID LOCATION_ID
+SELECT * FROM JOB; -- JOB_CODE로 동일함
+SELECT * FROM LOCATION; --            LOCAL_CODE
+SELECT * FROM SAL_GRADE; -- SAL_LEVEL 로 동일함
+
---
+++ 01_DQL(SELECT)/05_SUBQUERY_KH계정.sql
@@ -0,0 +1,332 @@
+/*
+    * 서브쿼리 (SUBQUERY)
+    - 하나의 SQL문 안에 포함된 또 다른 SELECT문
+    - 메인 SQL문을 위해 보조 역할을 하는 쿼리문
+*/
+
+-- 간단 서브쿼리 예시 1
+-- 노홍철 사원과 같은 부서에 속한 사원들 조회하고 싶음
+
+-- 1) 먼저 노옹철 사원의 부서코드 조회
+SELECT DEPT_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME = '노옹철';     --D9
+
+-- 2) 부서코드가 D9인 사원들 조회
+SELECT EMP_NAME
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D9';
+
+-- 위의 1, 2를 하나의 쿼리문으로..
+SELECT EMP_NAME
+FROM EMPLOYEE
+WHERE DEPT_CODE = (SELECT DEPT_CODE
+                   FROM EMPLOYEE
+                   WHERE EMP_NAME = '노옹철');
+                   
+-- 간단 서브쿼리 예시 2
+-- 전 직원의 평균급여보다 더 많은 급여를 받는 사원들의 사번, 이름, 직급코드, 급여 조회
+
+-- 1) 전 직원의 평균 급여 조회
+SELECT AVG(SALARY)
+FROM EMPLOYEE;      -- 대략 3047663원 인걸 알아냄
+
+-- 2) 급여가 3047663원 이상인 사원들 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY >= 3047663;
+
+-- 위의 1, 2를 하나의 쿼리문으로..
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY >= (SELECT AVG(SALARY)
+                 FROM EMPLOYEE);
+                
+---------------------------------------------------------------------------------
+/*
+    * 서브쿼리의 구분
+      서브쿼리 수행한 결과값이 몇 행 몇 열이냐에 따라서 분류됨
+      
+      - 단일행 서브쿼리 : 서브쿼리의 조회 결과값의 개수가 오로지 1개 일때 (한 행 한 열)
+      - 다중행 서브쿼리 : 서브쿼리의 조회 결과값이 여러행 일때 (여러행 한열) => 동명이인 노옹철
+      - 다중열 서브쿼리 : 서브쿼리의 조회 결과값이 한 행이지만 컬럼이 여러개일 때 (한 행 여러 열)
+      - 다중행 다중열 서브쿼리 : 서브쿼리 조회 결과값이 여러행 여러컬럼일 때 (여러행 여러열)
+      
+      >> 서브쿼리 종류가 뭐냐에 따라서 서브쿼리 앞에 붙는 연산자가 달라짐!! *** 중요 ***
+*/
+
+/*
+    1. 단일행 서브쿼리 (SINGLE ROW SUBQUERY)
+    서브쿼리의 조회 결과값의 개수가 오로지 1개일 때 (한행 한열)
+    일반 비교 연산자 사용 가능
+    =, !=, ^=, <>, <, >, >=, <= ...
+*/
+
+-- 1) 전 직원의 평균급여보다 급여를 더 적게 받는 사원들의 사원명, 직급코드, 급여 조회
+SELECT EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+--WHERE SALARY < 전직원의 평균 급여
+WHERE SALARY < (SELECT AVG(SALARY)
+                FROM EMPLOYEE);
+                
+-- 2) 최저 급여를 받는 사원의 사번, 이름, 급여, 입사일
+SELECT EMP_ID, EMP_NAME, SALARY, HIRE_DATE
+FROM EMPLOYEE
+--WHERE SALARY = 전직원들중 최저급여;
+WHERE SALARY = (SELECT MIN(SALARY)
+                FROM EMPLOYEE);
+                
+-- 3) 노옹철 사원의 급여보다 더 많이 받는 사원들의 사번, 이름, 부서코드, 급여 조회
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+--WHERE SALARY > 노옹철 사원의 급여
+WHERE SALARY > (SELECT SALARY
+                FROM EMPLOYEE
+                WHERE EMP_NAME = '노옹철');
+                
+-- >> 오라클 전용구문
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+AND SALARY > (SELECT SALARY
+              FROM EMPLOYEE
+              WHERE EMP_NAME = '노옹철');
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE SALARY > (SELECT SALARY
+              FROM EMPLOYEE
+              WHERE EMP_NAME = '노옹철');
+              
+-- 4) 부서별 급여합이 가장 큰 부서의 부서코드, 급여 합 조회
+-- 4_1) 먼저 부서별 급여합 중에서도 가장 큰 값 하나만 조회
+SELECT MAX(SUM(SALARY))
+FROM EMPLOYEE
+GROUP BY DEPT_CODE; --17700000
+
+-- 4_2) 부서별 급여합이 177700000원인 부서 조회 (부서코드, 급여합)
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING SUM(SALARY) = 17700000;
+
+-- 위의 두 단계를 하나의 쿼리문으로..
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY))
+                      FROM EMPLOYEE
+                      GROUP BY DEPT_CODE);
+                      
+-- 직접해보기
+-- 전지연 사원과 같은 부서원들의 사번, 사원명, 전화번호, 입사일, 부서명
+-- 단, 전지연은 제외!
+SELECT DEPT_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME = '전지연';  -- D1
+
+-- >> 오라클 전용 구문
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+AND DEPT_CODE = (SELECT DEPT_CODE
+                 FROM EMPLOYEE
+                 WHERE EMP_NAME = '전지연')
+AND EMP_NAME != '전지연';
+
+-- >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE DEPT_CODE = (SELECT DEPT_CODE
+                   FROM EMPLOYEE
+                   WHERE EMP_NAME = '전지연')
+AND EMP_NAME != '전지연';
+
+--================================================================================
+/*
+    2. 다중행 서브쿼리 (MULTI ROW SUBQUERY)
+    서브쿼리를 수행할 결과값이 여러 행 일때 (컬럼(열)의 한개)
+    
+    - IN 서브쿼리 : 여러개의 결과값 중에서 한개라도 일치하는 값이 있다면
+    
+    - > ANY 서브쿼리 : 여러개의 결과값 중에서 '한개라도' 클 경우 (여러개의 결과값 중에서 가장 작은 값 보다 클 경우)
+    - < ANY 서브쿼리 : 여러개의 결과값 중에서 '한개라도' 작을 경우 (여러개의 결과값 중에서 가장 큰 값 보다 작은 경우)
+
+    비교대상 > ANY (값1, 값2, 값3)
+    비교대상 > 값1 OR 비교대상 > 값2 OR 비교대상 > 값3
+    
+    - > ALL 서브쿼리 : 여러개의 '모든' 결과값들 보다 클 경우
+    - < ALL 서브쿼리 : 여러개의 '모든' 결과값들 보다 작을 경우
+    
+    비교대상 > ALL (값1, 값2, 값3)
+    비교대상 > 값1 AND 비교대상 > 값2 AND 비교대상 > 값3
+    
+*/
+
+-- 1) 유재식 또는 윤은해 사원과 같은 직급인 사원들의 사번, 사원명, 직급코드, 급여
+-- 1_1) 유재식 또는 윤은해 사원이 어떤 직급인지 조회
+SELECT JOB_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME IN ('유재식', '윤은해');   -- J3, J7
+
+-- 1_2) J3, J7인 직급의 사원들 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE JOB_CODE IN ('J3', 'J7');
+
+SELECT *
+FROM EMPLOYEE;
+
+-- 위의 두 단계를 하나의 쿼리문으로..
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE JOB_CODE IN (SELECT JOB_CODE
+                   FROM EMPLOYEE
+                   WHERE EMP_NAME IN ('유재식', '윤은해'));   -- = 이라고 쓰면 오류!! 여러 행으로 조회했기 때문
+                   -- 만약에 결과값이 여러 개 나올 것 같으면 그냥 안전하게 IN 으로 가자
+                   
+----------------------------------------
+-- 사원 => 대리 => 과장 => 차장 => 부장 ..
+-- 2) 대리 직급임에도 불구하고 과장 직급 급여들 중 최소 급여보다 많이 받는 직원 조회 (사번, 이름, 직급, 급여)
+
+-- 2_1) 먼저 과장 직급인 사원들의 급여 조회
+SELECT SALARY
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE = J.JOB_CODE
+AND J.JOB_NAME = '과장';  -- 2200000 2500000 3760000
+
+-- 2_2) 직급이 대리이면서 급여값이 위의 목록들 값 중에 하나라도 큰 사원
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+WHERE JOB_NAME = '대리'
+AND SALARY > ANY (2200000, 2500000, 3760000);
+
+-- 위의 두 단계를 하나의 쿼리문으로..
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+WHERE JOB_NAME = '대리'
+AND SALARY > ANY (SELECT SALARY
+                  FROM EMPLOYEE E, JOB J
+                  WHERE E.JOB_CODE = J.JOB_CODE
+                  AND J.JOB_NAME = '과장');
+
+-- 사실 단일행 서브쿼리로도 가능!
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+WHERE JOB_NAME = '대리'
+AND SALARY > (SELECT MIN(SALARY)
+                  FROM EMPLOYEE E, JOB J
+                  WHERE E.JOB_CODE = J.JOB_CODE
+                  AND J.JOB_NAME = '과장');
+                  
+-- 3) 과장 직급임에도 불구하고 차장 직급인 사원들의 모든 급여보다도 더 많이 받는 사원들의 사번, 이름, 직급명, 급여 조회
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE
+JOIN JOB USING (JOB_CODE)
+WHERE JOB_NAME = '과장'
+--AND SALARY > (차장 직급인 사람들의 급여들)
+AND SALARY > ALL (SELECT SALARY
+              FROM EMPLOYEE
+              JOIN JOB USING(JOB_CODE)
+              WHERE JOB_NAME = '차장');
+
+--=================================================================================
+/*
+    3. 다중열 서브쿼리
+    결과값은 한 행이지만 나열된 컬럼수가 여러개일 경우
+*/
+
+-- 1) 하이유 사원과 같은 부서코드, 같은 직급코드에 해당하는 사원들 조회 (사원명, 부서코드, 직급코드, 입사일자)
+-- 단일행 서브쿼리 ** 2개의 서브쿼리로 작성할 것!
+SELECT EMP_ID, DEPT_CODE, JOB_CODE, HIRE_DATE
+FROM EMPLOYEE
+WHERE DEPT_CODE = (SELECT DEPT_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '하이유')     -- D5
+AND JOB_CODE = (SELECT JOB_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '하이유');    -- J5
+
+
+-- >> 다중열 서브쿼리로
+SELECT EMP_ID, DEPT_CODE, JOB_CODE, HIRE_DATE
+FROM EMPLOYEE
+--WHERE (DEPT_CODE, JOB_CODE) = (하이유 사원의 부서코드 하이유 사원의 직급코드);
+WHERE (DEPT_CODE, JOB_CODE) = (SELECT DEPT_CODE, JOB_CODE
+                               FROM EMPLOYEE
+                               WHERE EMP_NAME = '하이유'); -- 순서 중요함!!, 개수 맞춰야함!!
+
+-- 박나라 사원과 같은 직급코드, 같은 사수를 가지고 있는 사원들의 사번, 사원명, 직급코드, 사수사번 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, MANAGER_ID
+FROM EMPLOYEE
+WHERE (JOB_CODE, MANAGER_ID) = (SELECT JOB_CODE, MANAGER_ID
+                                FROM EMPLOYEE
+                                WHERE EMP_NAME = '박나라');
+
+--==================================================================================
+/*
+    4. 다중행 다중열 서브쿼리
+    서브쿼리 조회 결과값이 여러행 여러열 인 경우
+*/
+
+-- 1) 각 직급별 최소 급여를 받는 사원 조회 (사번, 사원명, 직급코드, 급여)
+-- >> 각 직급별 최소 급여 조회
+SELECT JOB_CODE, MIN(SALARY)    -- 3
+FROM EMPLOYEE   -- 1
+GROUP BY JOB_CODE   -- 2
+
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE JOB_CODE = 'J2' AND SALARY = 3700000
+OR    JOB_CODE = 'J7' AND SALARY = 1380000
+...; -- 너무 많아..
+
+-- 서브쿼리를 적용해서 해보자
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE (JOB_CODE, SALARY) IN (SELECT JOB_CODE, MIN(SALARY)
+                            FROM EMPLOYEE
+                            GROUP BY JOB_CODE);
+
+-- 2) 각 부서별 최고 급여를 받는 사원 조회 (사번, 사원명, 직급코드, 급여)
+-- >> 각 부서별 최고 급여 조회
+SELECT DEPT_CODE, MAX(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE (DEPT_CODE, SALARY) IN (SELECT DEPT_CODE, MAX(SALARY)
+                              FROM EMPLOYEE
+                              GROUP BY DEPT_CODE);
+                              
+--===============================================================================
+/*
+    5. 인라인 뷰 (INLINE - VIEW)
+    
+    서브쿼리를 수행한 결과를 마치 테이블처럼 사용! ( WHERE 절에서 별칭 사용 가능!)
+*/
+
+-- 사원들의 사번, 이름, 보너스 포함 연봉 (별칭부여 : 연봉), 부서코드 조회 => 보너스 포함 연봉이 절대 NULL이 안나오게
+-- 단, 보너스 포함 연봉이 3000만원 이상인 사원들만 조회
+-- (급여 + 급여 * 보너스)*12
+SELECT EMP_NO, EMP_NAME, (SALARY + SALARY * NVL(BONUS, 0))*12 AS "연봉", DEPT_CODE   -- 3
+FROM EMPLOYEE   -- 1
+WHERE (SALARY + SALARY * NVL(BONUS, 0))*12 >= 30000000; -- 2
+
+SELECT EMP_NO, EMP_NAME, (SALARY + SALARY * NVL(BONUS, 0))*12 AS "연봉", DEPT_CODE
+FROM EMPLOYEE;
+-- 이 조회 결과를 마치 존재하는 테이블인 것 마냥 사용할 수 있음!! 그게 바로 인라인뷰!
+
+SELECT EMP_NO, EMP_NAME, 연봉, DEPT_CODE  -- ,MANAGER_ID 불가. 밑에 설정한 테이블에 없기 때문
+FROM (SELECT EMP_NO, EMP_NAME, (SALARY + SALARY * NVL(BONUS, 0))*12 AS "연봉", DEPT_CODE
+FROM EMPLOYEE)  -- 1
+WHERE 연봉 >= 30000000;   -- 2
+
+
